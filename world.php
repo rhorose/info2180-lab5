0f64785f5246edaa18f583534a55bb1e4e441b30
Rhoda Essien <rhorose@live.com>
2020-11-19 22:39:25 -0400

    Edited files

--- world.php
+++ world.php
@@ -10,12 +10,40 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
+if (isset()=$_GET['country'] || empty($_GET['country'])){
+
 $stmt= $conn->query("SELECT * FROM countries WHERE name LIKE '%country%'");
 $countries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<table>";
+echo "<tr><td>Name</td><td>Continent</td><td>Independence</td><td>Head of state</td></tr>\n ";
+
+foreach($results as $row){
+
+  echo "<tr><td>{$row['name']}</td><td>{$row['continent']}</td><td>{$row['independence_year']}</td><td>{$row['head_of_state']}</td></tr>\n ";
+}
+echo "</table>";
+
+
+}
+
+
+
+if (isset()=$_GET['cities']){
 $stmt= $conn->query("SELECT * FROM countries WHERE name LIKE '%country%' AND context=cities ");
 $cities = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+echo "<table>";
+echo "<tr><td>Name</td><td>District</td><td>Population</td></tr>\n ";
+
+foreach($results as $row){
+
+  echo "<tr><td>{$row['name']}</td><td>{$row['district']}</td><td>{$row['population']}</td></tr>\n ";
+}
+echo "</table>";
+
+}
 
 
 echo "<table>";
